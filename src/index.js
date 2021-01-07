86490c404021cb61960d59afde71e505b648459d
frankie <26461046+frankiekie@users.noreply.github.com>
2021-01-07 16:38:54 -0500

    Next big commit
    Adds new variables
    - elementMap
      - keeps track of elements that need to be loaded
    - preloadElementSet
      - if has size, load on pageshow
    - boolean flag isComplete
    - numerical-boolean flag isLoading
    
    rewrites of some arrow functions to minify better
    - added {} around arrow functions that
    don't need to return anything
    - thunk loadImage function with rAF
    
    implements beginning of IntersectionObserver
    - handleObservation
      - callback for IO
    - unveil
      - handles the actual unveiling of elements
    
    pygmyCore
    - adds queueImage to add images to proper Map and Set
      at start of function
    - assigns window DOMContentLoaded event
    based on preloadElementSet size
    - checks `loading="lazy"` support
      - if supported, loads every element in the map
      - then returns instance
    
    TODOS:
    - add config for ignoring `loading` attr flag
    - finish unveil function
      - can it be wrapped into loadImage fn?
    
    Current Sizes: based on UMD build
    Unminified (gz,br)
    1410b, 1237b
    
    Minified (gz,br)
    838b, 701b

--- src/index.js
+++ src/index.js
@@ -4,19 +4,33 @@
  * @typedef {import('..').pygmySizes} pygmySizes
  * @typedef {import('..').pygmyConfig} pygmyConfig
  */
-const config = Object.assign(window.pygmyConfig || {}, {
+
+const config = Object.assign({
 	selector: '[loading="lazy"]',
 	src: 'pygmy',
 	srcset: 'pygmyset',
 	sizes: 'pygmysizes',
 	preload: 'pygmyload',
-	observer: {
+	options: {
 		rootMargin: '0px',
 		threshold: 0,
-	}
-});
+	},
+}, window.pygmyConfig || {});
 
 let rpc = 0;
+
+const elementMap = new Map;
+const preloadElementSet = new Set;
+const observer = new IntersectionObserver(handleObservation, config.options)
+
+let isComplete = false,
+	isLoading = 0;
+
+/**
+ * @param {string} evtName
+ * @param {pygmyImage} image
+ */
+const dispatch = (evtName, image) => { image.element.dispatchEvent(new CustomEvent(evtName, { detail: image })) };
 
 /**
  * @param {any[]} arr
@@ -26,43 +40,83 @@ let rpc = 0;
 const chunk = (arr, size) => arr.reduce((acc, e, i) => (i % size ? acc[acc.length - 1].push(e) : acc.push([e]), acc), []);
 
 // @ts-ignore
-const raf = fn => (...args) => requestAnimationFrame(() => fn(...args));
-
+const raf = fn => (...a) => {
+	requestAnimationFrame(() => { fn(...a) })
+},
+	// @ts-ignore
+	ric = requestIdleCallback || ((cb, start = Date.now()) => setTimeout(_ => cb({ didTimeout: false, timeRemaining: _ => Math.max(0, 50 - (Date.now() - start)) }), 1));
 /**
  * @param {HTMLElement} element
  * @param  {...string} attrs 
  */
-const setAttr = (element, ...attrs) => chunk(attrs, 2).reduce((_, curr, i, a) => [i === 1 ? (element.setAttribute(_[0], _[1]), element.setAttribute(curr[0], curr[1])) : element.setAttribute(curr[0], curr[1])]);
+const setAttr = (element, ...attrs) => { chunk(attrs, 2).reduce((_, curr, i) => [i === 1 ? (element.setAttribute(_[0], _[1]), element.setAttribute(curr[0], curr[1])) : element.setAttribute(curr[0], curr[1])]) };
 
-/**  @param {pygmyImage} param */
-function loadImage({ element, isComplete }) {
-	setAttr(element, 'src', element.dataset[config.src], 'srcset', element.dataset[config.srcset] || '', 'sizes', element.dataset[config.sizes] || '');
+
+const loadImage = raf(/**  @param {HTMLImageElement} element */ element => {
+	setAttr(element,
+		'src', element.dataset[config.src],
+		'srcset', element.dataset[config.srcset] || '',
+		'sizes', element.dataset[config.sizes] || ''
+	);
+});
+
+/** @param {HTMLImageElement} element */
+function queueImage(element) {
+	const elementOptions = { rpc: ++rpc, isComplete: element.complete || false, element }
+	element.dataset.pygmyRpc = '' + rpc;
+
+	if (element.dataset[config.preload]) preloadElementSet.add(element)
+	elementMap.set(element, elementOptions);
+	observer.observe(element);
+	element.addEventListener('load', load);
+}
+
+/** @param {ProgressEvent} event */
+function load(event) {
+	const pygmyImage = elementMap.get(event.target);
+	pygmyImage.element.removeEventListener('load', load);
+	observer.unobserve(pygmyImage.element);
+
+	dispatch('pygmysizes:loaded', pygmyImage);
+}
+
+function unveil(element) {
+	isLoading++;
+	console.log(element);
 }
 
 /**
- * @param {string} selector
- * @returns Set<pygmyImage>
+ * @param {IntersectionObserverEntry[]} inViewEntries
+ * @param {IntersectionObserver} observer
  */
-function queueImages(selector) {
-	const elements = [...document.body.querySelectorAll(selector)].map((/** @type {HTMLImageElement} - element */ element) => ({ rpc: ++rpc, isComplete: element.complete || false, element }));
+function handleObservation(inViewEntries, observer) {
+	let i = 0,
+		l = inViewEntries.length;
 
-	// add delegated listener to window
+	for (; i < l; i++) {
+		if (!inViewEntries[i].isIntersecting) continue;
 
-	return new Set(elements);
+		unveil(inViewEntries[i].target);
+	}
 }
 
 function pygmySizesCore() {
-	const elements = queueImages(config.selector);
+	document.querySelectorAll(config.selector).forEach(queueImage);
+
+	if (preloadElementSet.size > 0) window.addEventListener('DOMContentLoaded', () => {
+		preloadElementSet.forEach(loadImage)
+	}, { once: true, capture: true });
+
+	// uncomment next line to test
+	delete HTMLImageElement.prototype.loading;
 
 	if ('loading' in HTMLImageElement.prototype) {
-		elements.forEach(raf(loadImage));
+		elementMap.forEach(loadImage);
 		return {
 			config,
-			elements,
+			elements: elementMap,
 		}
 	}
 }
 
-pygmySizes = pygmySizesCore();
-
-export default pygmySizes;
+export default pygmySizes = pygmySizesCore();;
