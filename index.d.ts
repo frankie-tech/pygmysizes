5bc40f2bc8ed51cacdcf4fa509887c06a31b2d79
frankie <26461046+frankiekie@users.noreply.github.com>
2021-01-07 11:39:06 -0500

    adds in new types for rewrites
    # @TODO remove old types

--- index.d.ts
+++ index.d.ts
@@ -1,47 +1,90 @@
-/** Config Interface */
-interface pygmyCfg {
-    dev: boolean;
-    selector: string;
-    offset: number;
-    state: pygmyStates;
-    attr: pygmyAttributes;
-    onLoad: boolean;
+/** Image */
+type pygmyImage = HTMLImageElement & {
+	isLoading: boolean;
 }
 
+/*
+{
+	selector: '[loading="lazy"]',
+	src: 'pygmy',
+	srcset: 'pygmyset',
+	sizes: 'pygmysizes',
+	preload: 'pygmyload',
+	observer: {
+		rootMargin: '0px',
+		threshold: 0,
+	}
+}
+*/
+
+interface pygmyCfg {
+	selector: string;
+	src: string;
+	srcset: string;
+	sizes: string;
+	preload: string;
+	observer: {
+		rootMargin: string;
+		threshold: number;
+	}
+}
+
+declare type pygmySizes = {
+	elements: Set<pygmyImage>;
+	config: pygmyCfg;
+}
+
+export {
+	pygmyImage,
+	pygmySizes
+}
+
+/** Config Interface */
+/*
+interface pygmyCfg {
+	dev: boolean;
+	selector: string;
+	offset: number;
+	state: pygmyStates;
+	attr: pygmyAttributes;
+	onLoad: boolean;
+}
+*/
+
 interface pygmyAttributes {
-    src: string;
-    srcset: string;
-    sizes: string;
+	src: string;
+	srcset: string;
+	sizes: string;
 }
 
 interface pygmyStates {
-    registered: string;
-    loading: string;
-    loaded: string;
-    fail: string;
+	registered: string;
+	loading: string;
+	loaded: string;
+	fail: string;
 }
 
-export { pygmyAttributes, pygmyCfg, pygmyStates };
+export { pygmyAttributes, pygmyStates };
 
 /** Internal Interface */
 
 interface utils {
-    warn: (msg: string, warn: boolean) => false;
-    empty: (test: any) => boolean;
-    emit: (el: HTMLElement, type: string, detail?: object) => HTMLElement;
-    inView: (el: IntersectionObserverEntry) => void;
-    id: () => string;
+	warn: (msg: string, warn: boolean) => false;
+	empty: (test: any) => boolean;
+	emit: (el: HTMLElement, type: string, detail?: object) => HTMLElement;
+	inView: (el: IntersectionObserverEntry) => void;
+	id: () => string;
 }
 
 interface internal extends utils {
-    intersecting: Event;
-    config: pygmyCfg;
-    elements: Array.<HTMLImageElement>;
-    loadingSupported: boolean;
-    remove: (el: HTMLElement, state: string) => HTMLElement;
-    getAttr: (el: HTMLElement, attr: string) => string | false;
-    set: (el: HTMLElement, state: string, val?: string, remove?: string) => HTMLElement;
-    merge: (target: pygmyCfg, ...sources: object[]) => pygmyCfg;
+	intersecting: Event;
+	config: pygmyCfg;
+	elements: Array.<HTMLImageElement>;
+	loadingSupported: boolean;
+	remove: (el: HTMLElement, state: string) => HTMLElement;
+	getAttr: (el: HTMLElement, attr: string) => string | false;
+	set: (el: HTMLElement, state: string, val?: string, remove?: string) => HTMLElement;
+	merge: (target: pygmyCfg, ...sources: object[]) => pygmyCfg;
 }
 
 
