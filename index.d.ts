852f35a2449caaa5ea9d77c5a820eb0894c85f10
frankie <26461046+frankiekie@users.noreply.github.com>
2021-01-10 18:18:36 -0500

    cleans type file
    removes old types that are no longer needed

--- index.d.ts
+++ index.d.ts
@@ -5,37 +5,25 @@ type pygmyImage = {
 	__element: HTMLImageElement;
 }
 
-/*
-{
-	selector: '[loading="lazy"]',
-	src: 'pygmy',
-	srcset: 'pygmyset',
-	sizes: 'pygmysizes',
-	preload: 'pygmyload',
-	observer: {
-		rootMargin: '0px',
-		threshold: 0,
-	}
-}
-*/
-
 type pygmyConfig = {
 	selector: string;
 	src: string;
 	srcset: string;
 	sizes: string;
 	preload: string;
-	rpc: number;
-	observer: {
-		rootMargin: string;
-		threshold: number;
+	rpc: string;
+	init?: boolean;
+	options?: {
+		root?: Element;
+		rootMargin?: string;
+		threshold?: number;
 	}
 }
 
 
 type pygmySizes = {
 	elements: Map<HTMLImageElement, pygmyImage>;
-	config: pygmyCfg;
+	config: pygmyConfig;
 }
 
 declare global {
@@ -51,55 +39,3 @@ export {
 	pygmySizes,
 	pygmyConfig
 }
-
-/** Config Interface */
-/*
-interface pygmyCfg {
-	dev: boolean;
-	selector: string;
-	offset: number;
-	state: pygmyStates;
-	attr: pygmyAttributes;
-	onLoad: boolean;
-}
-*/
-
-interface pygmyAttributes {
-	src: string;
-	srcset: string;
-	sizes: string;
-}
-
-interface pygmyStates {
-	registered: string;
-	loading: string;
-	loaded: string;
-	fail: string;
-}
-
-export { pygmyAttributes, pygmyStates };
-
-/** Internal Interface */
-
-interface utils {
-	warn: (msg: string, warn: boolean) => false;
-	empty: (test: any) => boolean;
-	emit: (el: HTMLElement, type: string, detail?: object) => HTMLElement;
-	inView: (el: IntersectionObserverEntry) => void;
-	id: () => string;
-}
-
-interface internal extends utils {
-	intersecting: Event;
-	config: pygmyCfg;
-	elements: Array.<HTMLImageElement>;
-	loadingSupported: boolean;
-	remove: (el: HTMLElement, state: string) => HTMLElement;
-	getAttr: (el: HTMLElement, attr: string) => string | false;
-	set: (el: HTMLElement, state: string, val?: string, remove?: string) => HTMLElement;
-	merge: (target: pygmyCfg, ...sources: object[]) => pygmyCfg;
-}
-
-
-
-export { utils, internal };
