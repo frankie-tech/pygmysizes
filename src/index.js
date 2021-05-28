a132b542078fed2ecf92112d0fa8cec2b6c58a90
Frankie <26461046+frankie-tech@users.noreply.github.com>
2021-05-27 20:58:26 -0400

    moved around some code to get it to compress better
    swapped out setTimeout for requestAnimationFrame
    
    sets some code variables in places it maybe _shouldn't_
    be set... but whatever, tsc can't stop me
    
    changes some places where I was able to pass in the element
    into the function rather than pull it out of the
    pygmyImage object
    
    in general didn't assign variables where it wasn't needed

--- src/index.js
+++ src/index.js
@@ -14,87 +14,96 @@ let { src, srcset, sel, preload, init, options } = Object.assign({
 	init: true,
 	options: { threshold: 0, },
 	// @ts-ignore
-}, self.pygmyConfig || {}),
-	/** @type {Map<HTMLImageElement, pygmyImage>} */
-	elementMap = new Map;
+}, self.pygmyConfig || {});
+/** @type {Map<(HTMLElement|HTMLImageElement|EventTarget), pygmyImage>} elementMap */
+let elementMap = new Map,
+	raf = requestAnimationFrame;
 
 let observer = new IntersectionObserver(
 /** @param {IntersectionObserverEntry[]} inViewEntries */ inViewEntries => {
-		for (let l = inViewEntries.length, e, pygmy; l--;)
-			(e = inViewEntries[l]).isIntersecting && (pygmy = elementMap.get(e.target)) && unveil(pygmy)
+		for (let l = inViewEntries.length, e; l--;)
+			(e = inViewEntries[l]).isIntersecting
+				// @ts-ignore
+				&& unveil(elementMap.get(e.target), e.target)
 	}, options);
 
 /**
  * @param {pygmyImage} pygmy
+ * @param {HTMLImageElement} _el
  * @param {(0|1|2|3)} to 
- * @param {(0|1|2|3)} from 
+ * @param {(0|1|2|3)} [from]
  * @param {string} [t]
  * @param {(k:(0|1|2|3)) => string} [state]
  */
-let set = (pygmy, to, from, t, state) => {
+let set = (pygmy, _el, to, from, t, state) => {
 	/** @param {(0|1|2|3)} key */
 	state = key => 'pygmy' + ['Before', 'Load', 'Preload'][key];
+	if (from !== to) delete _el.dataset[state(from || 0)];
 	// @ts-ignore
-	pygmy._el.dataset[t = state(to)] = pygmy._el.dispatchEvent(new CustomEvent(t, { detail: pygmy }));
-	if (from !== to) delete pygmy._el.dataset[state(from)];
+	_el.dataset[t = state(to)] = _el.dispatchEvent(new CustomEvent(t, { detail: pygmy }));
 }
 
 /**
  * @param {HTMLImageElement} _el
+ * @param {boolean} [isPreload]
+ * @param {object} [pygmy]
  */
-let queueImage = (_el) => {
-	let isPreload = preload in _el.dataset,
-		/** @type {pygmyImage} */
-		pygmy = {
-			_el,
-			preload: isPreload,
-		};
+let queueImage = (_el, isPreload, pygmy) => {
+	elementMap.set(_el, pygmy = {
+		_el: _el,
+		preload: isPreload = preload in _el.dataset,
+	});
 
-	elementMap.set(_el, pygmy);
 	_el.onload = loadPygmy;
-	isPreload ?
-		setTimeout(preloadPygmy, 0, pygmy)
-		: observer.observe(_el);
+	observer.observe(_el,
+		// I hate hiding this inside of the observer.observe
+		// but it saves between 6-18b, sooooo...
+		// And for some reason
+		// organizing it like this
+		// unveil / set / pygmy._el.loading saves another 3b ???
+		// @ts-ignore
+		isPreload && raf(_ => {
+			// set [loading] to the rAF rpc instead of an empty string (saves 1b)
+			unveil(pygmy, _el);
+			set(pygmy, _el, 2);
+			// Also doesn't like assigning a number to [loading=""] when it should be a string
+			// @ts-ignore
+			pygmy._el.loading = _
+		}));
 }
 
 /**
  * @param {Event} e 
  * @property {HTMLImageElement} e.target
+ * @param {pygmyImage} [pygmy]
+ * @param {HTMLImageElement} [_el]
 */
-let loadPygmy = e => {
+let loadPygmy = (e, pygmy, _el) => {
 	// @ts-ignore
-	let pygmy = elementMap.get(e.target),
-		_el = pygmy._el;
+	observer.unobserve(
+		(_el = (
+			pygmy = elementMap.get(e.target)
+		)._el)
+	);
+
+	set(pygmy, _el, pygmy.done = 1);
 	_el.removeEventListener('load', loadPygmy);
-
-	observer.unobserve(_el);
-
-	set(pygmy, 1, 0);
-	pygmy.done = true;
 	elementMap.set(_el, pygmy);
 }
-/** @param {pygmyImage} pygmy */
-let preloadPygmy = pygmy => {
-	pygmy._el.loading = '';
-	unveil(pygmy);
-	set(pygmy, 2, 0);
-}
 
-/** @param {pygmyImage} pygmy */
-let unveil = (pygmy) => {
-	pygmy._el.src = pygmy._el.dataset[src];
-	// _el.sizes = _el.dataset[sizes] || '';
-	pygmy._el.srcset = pygmy._el.dataset[srcset] || '';
+/**
+ * @param {pygmyImage} pygmy
+ * @param {HTMLImageElement} _el
+ */
+let unveil = (pygmy, _el) => raf(_ => set(pygmy, _el, 0),
+	// @ts-ignore tsc doesn't like the commas and thinks that these are extra arguments
+	_el.src = _el.dataset[src], _el.srcset = _el.dataset[srcset] || '')
 
-	set(pygmy, 0, 0);
-}
-
-/** @param {string} sel */
-let pygmySizes = sel =>
+/** @param {number} _ */
+let pygmySizes = _ =>
 	// @ts-ignore
 	document.querySelectorAll(sel).forEach(queueImage);
 
-init && setTimeout(pygmySizes, 0, sel);
-
+init && raf(pygmySizes);
 
 export default pygmySizes
